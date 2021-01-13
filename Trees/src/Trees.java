2a3928bcdbb1eaca840729265ead4d93a1555b0e
Precious <preciousjatau@ou.edu>
2021-01-12 20:47:22 -0600

    added contains() method and helper

--- Trees/src/Trees.java
+++ Trees/src/Trees.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 
 /**
  * Class for binary trees
@@ -10,7 +11,12 @@
  * 1. Node class (completed)
  * 2. add (completed)
  * 3. BFS(in progress)
- * 
+ * 4. contains (in progress)
+ * 5. remove
+ * 6. leftRotate
+ * 7. rightRotate
+ * 8. leftRightRotate
+ * 9. rightLeftRotate
  * */
 
 public class Trees<T>{
@@ -80,13 +86,97 @@ public class Trees<T>{
 		
 	}
 	
-	public void BFS(Node<T> node) {
+	
+	/** Checks whether tree contains a given node
+	 * @param target. target node.
+	 * @return true/false indicating if target not was found/not.
+	 * */
+	public boolean contains(Node<T> target) {
 		
-			
 		
+		return contains(target, root);
+	}
+	
+	
+	/** Helper method for contains
+	 * @param target. target node.
+	 * @param base. root of current tree/subtree.
+	 * */
+	public boolean contains(Node<T> target, Node<T> base) {
+		
+		// check if base node is empty
+		if (base == null)
+			return false;
+		
+		// check if base node's data match target's data
+		if (((Comparable<T>) target.data).compareTo(base.data) == 0) // target found 
+		{
+			return true;
+		}
+		
+		// check right child
+		if (((Comparable<T>) target.data).compareTo(base.data) > 0) // target found 
+		{
+			return contains(target,base.right);
+		}
+		
+		// check left child
+		return contains(target, base.left);
 		
 	}
 	
 	
+	/**Performs Breadth First Search (BFS) on the tree.
+	 * @param start. Node from which to start BFS
+	 * @param target. Node to be found.
+	 * @return node list for path traversed from start to target or null is target node is not found.
+	 * */
+	public void BFS(Node<T> node) {
+		
+		LinkedList<Node<T>> queue = new LinkedList<Node<T>>();
+		
+				
+			
+	}
+	
+	/*
+	public void BFSHelper(Node<T> node, LinkedList<Node<T>> queue) {
+		
+		// base case
+		if (node == null)
+			return;
+		
+		queue.add(node);
+				
+		while (!queue.isEmpty()) {
+			// remove, print current data
+			node = queue.poll();
+			System.out.print(node.data);
+					
+			// add all neighbors
+			queue.add(node.left);
+			queue.add(node.right);
+			}
+	}
+	*/
+	
+	public static void main(String[] args) 
+	{
+		LinkedList<Integer> a = new LinkedList<Integer> ();
+		a.addFirst(1);
+		a.addLast(3);
+		a.addFirst(6);
+		// 6,1,3		
+		
+		
+		System.out.println("Testing main.");
+		
+		for (int i:a) 
+		{
+			System.out.println(i);
+		}
+		
+	}
+
 
 }
